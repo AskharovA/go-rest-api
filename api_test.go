4fc4e9ef5d530177e6ea3359e6cfc6c74c735539
Akylbek Askharov <androidepta@gmail.com>
2025-10-10 07:53:09 +0500

    added event creation tests

--- api_test.go
+++ api_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"AskharovA/go-rest-api/models"
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -17,12 +21,43 @@ func setupRouter() *gin.Engine {
 }
 
 func TestGetEvents(t *testing.T) {
-	server := setupRouter()
+	router := setupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/events", nil)
 
-	server.ServeHTTP(w, req)
+	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestCreateEvent(t *testing.T) {
+	router := setupRouter()
+
+	newEvent := models.Event{
+		Name:        "Test Event",
+		Description: "A test event.",
+		Location:    "Test Location",
+		DateTime:    time.Now(),
+	}
+	payload, _ := json.Marshal(newEvent)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/events", bytes.NewBuffer(payload))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	var responseBody map[string]interface{}
+	err := json.Unmarshal(w.Body.Bytes(), &responseBody)
+	assert.Nil(t, err)
+
+	eventData, ok := responseBody["data"].(map[string]interface{})
+	assert.True(t, ok)
+
+	assert.Equal(t, newEvent.Name, eventData["name"])
+	assert.Equal(t, newEvent.Description, eventData["description"])
+	assert.Equal(t, float64(1), eventData["userId"])
 }
--- models/event.go
+++ models/event.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Event struct {
-	ID          int
-	Name        string    `binding:"required"`
-	Description string    `binding:"required"`
-	Location    string    `binding:"required"`
-	DateTime    time.Time `binding:"required"`
-	UserID      int
+	ID          int       `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"dateTime" binding:"required"`
+	UserID      int       `json:"userId"`
 }
 
 var events = []Event{}
