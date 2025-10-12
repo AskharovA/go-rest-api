0ddee27a2245d0e2bc55e2b044ccc2ee8f60153c
Akylbek Askharov <androidepta@gmail.com>
2025-10-12 10:45:51 +0500

    added gradeful shutdown & fix error handling

--- main.go
+++ main.go
@@ -4,33 +4,64 @@ import (
 	"AskharovA/go-rest-api/config"
 	"AskharovA/go-rest-api/db"
 	"AskharovA/go-rest-api/routes"
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter(dbConn *sql.DB) *gin.Engine {
-	server := gin.Default()
-	routes.RegisterRoutes(server, dbConn)
+	router := gin.Default()
+	routes.RegisterRoutes(router, dbConn)
 
-	return server
+	return router
 }
 
 func main() {
 	dbConn, err := db.InitDB("api.db")
 	if err != nil {
-		panic("Could not connect to database.")
+		log.Fatalf("Could not connect to database: %v", err)
 	}
+
 	defer dbConn.Close()
 
 	err = db.CreateTables(dbConn)
 	if err != nil {
-		panic("Could not create tables.")
+		log.Fatalf("Could not create tables: %v", err)
 	}
 
-	server := setupRouter(dbConn)
+	router := setupRouter(dbConn)
 
-	addr := fmt.Sprintf(":%d", config.Get().AppPort)
-	server.Run(addr)
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", config.Get().AppPort),
+		Handler: router,
+	}
+
+	go func() {
+		log.Printf("Server starting on port %d\n", config.Get().AppPort)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
